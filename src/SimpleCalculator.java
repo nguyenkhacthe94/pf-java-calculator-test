249624963e1bbfc19583aa73ce9910bfd1e54560
nguyenkhacthe94 <nguyenkhacthe94@gmail.com>
2018-03-28 10:38:19 +0700

    Calculator test

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,13 @@
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        int sum = first+second;
+        return sum;
+    }
+
+    public static int sub(int first, int second){
+        int minus = first - second;
+        return minus;
+    }
+}
+
+
